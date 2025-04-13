4fe1ac9959c2952d33db488123a903e6168b89b1
Kairavipatra <166865579+Kairavipatra@users.noreply.github.com>
2025-04-13 22:59:23 +0530

    app.py

--- app/routes/app.py
+++ app/routes/app.py
@@ -1,10 +1,7 @@
-from flask import Flask, render_template
+from flask import Blueprint, render_template
 
-app_bp = Flask(__name__)
+app_bp = Blueprint('app', __name__)
 
 @app_bp.route('/')
 def home():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True, port=10000)
+     return render_template('index.html')
