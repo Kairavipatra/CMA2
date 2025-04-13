21410aadc24b8227d2cb283a3ea956ec20cbdc4c
Kairavipatra <166865579+Kairavipatra@users.noreply.github.com>
2025-04-14 01:05:59 +0530

    __init__.py

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    from app.routes import auth, products, appointments, dog_walking, toys
-    from .routes import app as app_routes
- 
+    from app.routes import app_bp 
+    from app.routes import auth, products, appointments, dog_walking, toys, app as app_routes
+    app.register_blueprint(app_bp)
     app.register_blueprint(auth.auth_bp)
     app.register_blueprint(products.products_bp)
     app.register_blueprint(appointments.appointments_bp)
