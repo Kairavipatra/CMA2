7c041418953e90e64487278cce70a0e5af5388ce
Kairavipatra <166865579+Kairavipatra@users.noreply.github.com>
2025-04-14 01:39:49 +0530

    app.py

--- app/routes/app.py
+++ app/routes/app.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template
 
 app_bp = Blueprint('app', __name__)
 
-@app_bp.route('/index.html')
+@app_bp.route('/')
 def home():
     return render_template('index.html')
 
