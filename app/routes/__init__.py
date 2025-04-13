f6ff44d5ba512c4a864ecd775acc9864e0911000
Kairavipatra <166865579+Kairavipatra@users.noreply.github.com>
2025-04-13 22:56:11 +0530

    __init__.py

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,14 +2,19 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.secret_key = 'supersecretkey'  # for session & forms
 
-    from app.routes import auth, products, appointments, dog_walking, toys
+    from .routes import app as app_routes
+    from .routes import appointments
+    from .routes import auth
+    from .routes import dog_walking
+    from .routes import products
+    from .routes import toys
 
-    app.register_blueprint(auth.bp)
-    app.register_blueprint(products.bp)
-    app.register_blueprint(appointments.bp)
-    app.register_blueprint(dog_walking.bp)
-    app.register_blueprint(toys.bp)
+    app.register_blueprint(app_routes.app_bp)
+    app.register_blueprint(appointments.appointments_bp)
+    app.register_blueprint(auth.auth_bp)
+    app.register_blueprint(dog_walking.dog_walking_bp)
+    app.register_blueprint(products.products_bp)
+    app.register_blueprint(toys.toys_bp)
 
     return app
