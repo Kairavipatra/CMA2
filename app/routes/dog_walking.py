595d84ce7496f37f81b9a72bc7f63a4cc9f7c528
Kairavipatra <166865579+Kairavipatra@users.noreply.github.com>
2025-04-13 22:52:01 +0530

    dog_walking.py

--- app/routes/dog_walking.py
+++ app/routes/dog_walking.py
@@ -1,10 +1,10 @@
 from flask import Blueprint, request, jsonify
 
-bp = Blueprint('dog_walking', __name__, url_prefix='/dog-walking')
+dog_walking_bp = Blueprint('dog_walking', __name__, url_prefix='/dog-walking')
 
 walk_schedule = []
 
-@bp.route('/schedule', methods=['POST'])
+@dog_walking_bp.route('/schedule', methods=['POST'])
 def schedule_walk():
     data = request.json
     walk_schedule.append(data)
