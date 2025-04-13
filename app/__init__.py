e4aa18ad63ef6dd89935e5f8d34ee43c67bfb3de
Kairavipatra <166865579+Kairavipatra@users.noreply.github.com>
2025-04-14 01:12:40 +0530

    __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -18,14 +18,17 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    from app.routes import app_bp 
-    from app.routes import auth, products, appointments, dog_walking, toys, app as app_routes
-    app.register_blueprint(app_bp)
+
+    # Import all blueprints
+    from app.routes import app as app_routes
+    from app.routes import auth, products, appointments, dog_walking, toys
+
+    # Register blueprints
+    app.register_blueprint(app_routes.app_bp)  # Your general pages like /, /blog etc.
     app.register_blueprint(auth.auth_bp)
     app.register_blueprint(products.products_bp)
     app.register_blueprint(appointments.appointments_bp)
     app.register_blueprint(dog_walking.dog_walking_bp)
     app.register_blueprint(toys.toys_bp)
-    app.register_blueprint(app_routes.app_bp)
 
     return app
