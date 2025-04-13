e8b8ccb1c81d775ed3c240480a711aca6c520595
Kairavipatra <166865579+Kairavipatra@users.noreply.github.com>
2025-04-14 00:40:19 +0530

    run.py

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))  # Render will set PORT env
+    port = int(os.environ.get('PORT', 10000))  # Render injects PORT env variable.
     app.run(host='0.0.0.0', port=port)
