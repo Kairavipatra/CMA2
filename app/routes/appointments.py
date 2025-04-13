8fa9c085d0c91415fb8cbf090bc4fb135155a990
Kairavipatra <166865579+Kairavipatra@users.noreply.github.com>
2025-04-13 22:51:17 +0530

    appointments.py

--- app/routes/appointments.py
+++ app/routes/appointments.py
@@ -1,10 +1,10 @@
 from flask import Blueprint, request, jsonify
 
-bp = Blueprint('appointments', __name__, url_prefix='/appointments')
+appointments_bp = Blueprint('appointments', __name__, url_prefix='/appointments')
 
 appointments = []
 
-@bp.route('/book', methods=['POST'])
+@appointments_bp.route('/book', methods=['POST'])
 def book():
     data = request.json
     appointments.append(data)
