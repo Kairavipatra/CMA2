7f1f6f09c0f490330f0c4d2bcc8a45fc69ccc776
Kairavipatra <166865579+Kairavipatra@users.noreply.github.com>
2025-04-14 15:35:26 +0530

    models.py

--- app/models.py
+++ app/models.py
@@ -22,7 +22,13 @@ class User(db.Model, UserMixin):
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(300), nullable=True)
     price = db.Column(db.Float, nullable=False)
+    category = db.Column(db.String(50), nullable=False)  # "Food" or "Toys"
+
+    def __repr__(self):
+        return f'<Product {self.name}>'
+
   
   
 
