81137938d480bb1e37367f17ace4c316671152ef
Kairavipatra <166865579+Kairavipatra@users.noreply.github.com>
2025-04-14 13:32:59 +0530

    models.py

--- app/models.py
+++ app/models.py
@@ -1,9 +1,19 @@
 from . import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
+
+    def set_password(self, password):
+        """Hashes and sets the user's password."""
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        """Checks if the provided password matches the hashed password."""
+        return check_password_hash(self.password, password)
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
