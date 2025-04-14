ea0fbea8c352863ec31632c4bfdb6f892e2ca5f7
Kairavipatra <166865579+Kairavipatra@users.noreply.github.com>
2025-04-14 13:14:21 +0530

    run.py

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 import os
-from app import create_app
-from app import db
-db.create_all()
+from app import create_app, db
+
 app = create_app()
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))  # Render injects PORT env variable.
